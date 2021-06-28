6ede541a606606d37dde51b2f2b2966b580f7bfd
Sodara Y <ysodara89@gmail.com>
2021-06-27 18:53:42 -0700

    finished with commenting

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -3,16 +3,32 @@ package main;
 import java.util.Scanner;
 
 public class Converter {
-		
-	public double volumeConversions(int options) {
+	
+	/***
+	 * Volume Conversions: this third layer that ask user for quantity to convert, 
+	 * print out the result after conversion 
+	 * @param options as integer
+	 * @return NONE
+	 */
+	public void volumeConversions(int options) {
+		//Initialize object for invoking non static method.
 		Converter md2 = new Converter();
+		//Scanner object
 		Scanner scanner= new Scanner(System.in);
+		
+		//Quantity of Teaspoons
 		double teaspoons = 0d;
+		
+		//result variable for return value from conversion
 		double result = 0d;
 		
+		//Switch check if options 1 or 2
+		//if 1, invoke teaspoonsToTablespoons()
+		//if 2, invoke teaspoonsToCups()
 		switch (options) {
 		case 1:{
 			System.out.println("Enter Teaspoons: ");
+			//Get user input
 			teaspoons = scanner.nextDouble();
 			result = md2.teaspoonsToTablespoons(teaspoons);
 			System.out.println("\n"+(int)teaspoons+" Teaspoons is equl to "+ result + " Tablespoons\n");
@@ -20,22 +36,31 @@ public class Converter {
 		}
 		case 2:{
 			System.out.println("Enter Teaspoons: ");
+			//Get user input
 			teaspoons = scanner.nextDouble();
-			result= md2.teasponsToCups(teaspoons);
+			result= md2.teaspoonsToCups(teaspoons);
 			System.out.println("\n"+(int)teaspoons+" Teaspoons is equl to "+ result + " Cups\n");
 			break;
 		}
 		
 		}
-		return result;
 	}
-		
+	
+	/***
+	 * Distance Conversions: this third layer that ask user for quantity to convert, 
+	 * print out the result after conversion
+	 * @param options  as integer
+	 */
 	public void distanceConversions(int options) {
 		Converter md2 = new Converter();
 		Scanner scanner= new Scanner(System.in);
 		
 		double userInput = 0d;
 		double result;
+		
+		//Switch check if options 1 or 2
+		//if 1, invoke feetToMeters()
+		//if 2, invoke milesToKilometers()
 		switch (options) {
 		case 1:{
 			System.out.println("Enter Feet: ");
@@ -55,34 +80,62 @@ public class Converter {
 		
 	}
 	
+	/***
+	 * Teaspoons To Tablespoons: this function convert teaspoons to tablespoons based on user input
+	 * @param teaspoons as double
+	 * @return result of Teaspoons To Tablespoons
+	 */
 	public double teaspoonsToTablespoons(double teaspoons) {
 		double teaspoonPerTablespoon = 0.333333d;
 		
 		return teaspoons * teaspoonPerTablespoon;
 	}
 	
-	public double teasponsToCups(double teaspoons) {
+	/***
+	 * Teaspoons To Cups : this function convert teaspoons to cups based on user input
+	 * @param teaspoons as double
+	 * @return result of Teaspoons To Cups
+	 */
+	public double teaspoonsToCups(double teaspoons) {
 		double teaspoonPerCups = 0.0208333;
 		
 		return teaspoons * teaspoonPerCups;
 	}
 	
+	/***
+	 * Feet To Meters : this function convert feet to meters based on user input
+	 * @param feet as double
+	 * @return result of Feet To Meters
+	 */
 	public double feetToMeters(double feet) {
 		double feetPerMeter = 0.3048d;
 		return feet * feetPerMeter;
 	}
 	
+	/***
+	 * Miles To Kilometers : this function convert miles to kilometers based on user input
+	 * @param miles as double
+	 * @return result of miles To kilometers
+	 */
 	public double milesToKilometers(double miles) {
 		double milesPerKilometers = 1.60934d;
 		return miles * milesPerKilometers;
 	}
 	
 	public static void main(String[] args) {
+		
 		Converter md = new Converter();
 		
 		int menuSelection = 0;
 		double result = 0d;
 		Scanner scanner= new Scanner(System.in);
+		/***
+		 * While loop checks for menu selection from user input
+		 * if 1, invoke volumeConversions
+		 * if 2, invoke distanceConversions
+		 * if 3, exit the program
+		 * if everything else, re-run the program
+		 */
 		while (menuSelection != 3) {
 			
 			System.out.println("Please select an option:");
@@ -112,10 +165,12 @@ public class Converter {
 				md.distanceConversions(distanceSeletions);
 				break;
 			}
+			
 			case 3: {
 				System.out.println("You have exited the program.");
 				break;
 			}
+			
 			default: {
 				System.out.println("Please select option 1 to 4");
 				break;
